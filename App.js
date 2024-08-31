71c8eb6ac1ae088bbf943ddc25404e0a90c29a08
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-08-31 17:09:27 +0700

    refactor: project structure

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import ProfilePage from "./components/ProfilePage";
-import HomePage from "./components/HomePage";
+import ProfilePage from "./screens/ProfilePage";
+import HomePage from "./screens/HomePage";
 
 const Stack = createNativeStackNavigator();
 
